f694c3f3c382b24b80b761da65ad3471b7f16d87
serori <serori328@gmail.com>
2017-01-23 04:31:16 +0900

    コミットアーンドプーーーーーーーーーーーーーーーーーーーーーーーッシュ！！！！！！！！！！！！！！！！！

--- app/src/main/java/net/ketc/btsdemocalc/MyApp.kt
+++ app/src/main/java/net/ketc/btsdemocalc/MyApp.kt
@@ -7,27 +7,27 @@ import net.orekyuu.bts4j.BugReportBuilder
 import net.orekyuu.bts4j.BugReportServiceBuilder
 
 class MyApp : Application() {
-    val service = BugReportServiceBuilder("url", "token").build()!!
+    val service = BugReportServiceBuilder("http://192.168.10.5:18080", "06a2736a-31ca-4450-8b25-5A3AD6A724D8").build()!!
     override fun onCreate() {
         super.onCreate()
         val defaultUncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler()
         Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
             val throwableStr = throwable.javaClass.simpleName
             val device = Build.DEVICE
-            val androidCodeName = Build.VERSION.CODENAME
             val apiLevel = Build.VERSION.SDK_INT
             val build = BugReportBuilder()
-                    .stacktrace(throwable.stackTrace.joinToString { "\n" })
+                    .stacktrace(throwableStr + "\n" + throwable.stackTrace.joinToString(separator = "\n") { "    at $it" })
                     .description("$throwableStr : $thread")
                     .title(throwableStr)
-                    .version("version")
-                    .runtimeInfo("device = $device os = $androidCodeName api = $apiLevel")
-                    .log("log")
+                    .version(BuildConfig.VERSION_NAME)
+                    .runtimeInfo("device = $device, api = $apiLevel")
+                    .log("it is log")
                     .build()
             AsyncTask.execute {
                 try {
                     service.sendReport(build)
                 } catch (e: Throwable) {
+                    e.printStackTrace()
                 } finally {
                     defaultUncaughtExceptionHandler.uncaughtException(thread, throwable)
                 }
